37752f5123198e6bb495fceaddfacacdc17883db
apollowebdesigns <andrewevans0@outlook.com>
2018-11-11 14:27:10 +0000

    added in new server file - could go belly up

---
+++ mlserver.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+"""
+Creates an HTTP server with basic auth and websocket communication.
+"""
+import argparse
+import base64
+import hashlib
+import os
+import time
+import threading
+import webbrowser
+import cv2
+import picamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import tensorflow as tf
+import sys
+import numpy as np
+
+# Set up camera constants
+IM_WIDTH = 1280
+IM_HEIGHT = 720
+#IM_WIDTH = 640    Use smaller resolution for
+#IM_HEIGHT = 480   slightly faster framerate
+
+# Select camera type (if user enters --usbcam when calling this script,
+# a USB webcam will be used)
+camera_type = 'picamera'
+parser = argparse.ArgumentParser()
+parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
+                    action='store_true')
+args = parser.parse_args()
+if args.usbcam:
+    camera_type = 'usb'
+
+# This is needed since the working directory is the object_detection folder.
+sys.path.append('..')
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+
+PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+try:
+    import cStringIO as io
+except ImportError:
+    import io
+
+import tornado.web
+import tornado.websocket
+from tornado.ioloop import PeriodicCallback
+
+# Hashed password for comparison and a cookie for login cache
+ROOT = os.path.normpath(os.path.dirname(__file__))
+with open(os.path.join(ROOT, "password.txt")) as in_file:
+    PASSWORD = in_file.read().strip()
+COOKIE_NAME = "camp"
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        if args.require_login and not self.get_secure_cookie(COOKIE_NAME):
+            self.redirect("/login")
+        else:
+            self.render("index.html", port=args.port)
+
+
+class LoginHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        password = self.get_argument("password", "")
+        if hashlib.sha512(password).hexdigest() == PASSWORD:
+            self.set_secure_cookie(COOKIE_NAME, str(time.time()))
+            self.redirect("/")
+        else:
+            time.sleep(1)
+            self.redirect(u"/login?error")
+
+
+class ErrorHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.send_error(status_code=403)
+
+
+class WebSocket(tornado.websocket.WebSocketHandler):
+
+    def on_message(self, message):
+        """Evaluates the function pointed to by json-rpc."""
+
+        # Start an infinite loop when this is called
+        if message == "read_camera":
+            if not args.require_login or self.get_secure_cookie(COOKIE_NAME):
+                self.camera_loop = PeriodicCallback(self.loop, 10)
+                self.camera_loop.start()
+            else:
+                print("Unauthenticated websocket request")
+
+        # Extensibility for other methods
+        else:
+            print("Unsupported function: " + message)
+
+    def loop(self):
+        camera = PiCamera()
+        camera.resolution = (IM_WIDTH,IM_HEIGHT)
+        camera.framerate = 10
+        rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
+        rawCapture.truncate(0)
+
+        for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+
+            t1 = cv2.getTickCount()
+            
+            # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+            # i.e. a single-column array, where each item in the column has the pixel RGB value
+            frame = frame1.array
+            frame.setflags(write=1)
+            frame_expanded = np.expand_dims(frame, axis=0)
+
+            # Perform the actual detection by running the model with the image as input
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: frame_expanded})
+
+            # Draw the results of the detection (aka 'visulaize the results')
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                frame,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8,
+                min_score_thresh=0.40)
+
+            cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+
+            # All the results have been drawn on the frame, so it's time to display it.
+            cv2.imshow('Object detector', frame)
+
+            t2 = cv2.getTickCount()
+            time1 = (t2-t1)/freq
+            frame_rate_calc = 1/time1
+
+            # Press 'q' to quit
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+            rawCapture.truncate(0)
+
+        camera.close()
+
+
+        """Sends camera images in an infinite loop."""
+        sio = io.BytesIO()
+
+        if args.use_usb:
+            _, frame = camera.read()
+            img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            img.save(sio, "JPEG")
+        else:
+            camera.capture(sio, "jpeg", use_video_port=True)
+
+        try:
+            self.write_message(base64.b64encode(sio.getvalue()))
+        except tornado.websocket.WebSocketClosedError:
+            self.camera_loop.stop()
+
+
+parser = argparse.ArgumentParser(description="Starts a webserver that "
+                                 "connects to a webcam.")
+parser.add_argument("--port", type=int, default=8000, help="The "
+                    "port on which to serve the website.")
+parser.add_argument("--resolution", type=str, default="low", help="The "
+                    "video resolution. Can be high, medium, or low.")
+parser.add_argument("--require-login", action="store_true", help="Require "
+                    "a password to log in to webserver.")
+parser.add_argument("--use-usb", action="store_true", help="Use a USB "
+                    "webcam instead of the standard Pi camera.")
+parser.add_argument("--usb-id", type=int, default=0, help="The "
+                     "usb camera number to display")
+args = parser.parse_args()
+
+if args.use_usb:
+    from PIL import Image
+    camera = cv2.VideoCapture(args.usb_id)
+else:
+    camera = picamera.PiCamera()
+    camera.start_preview()
+
+resolutions = {"high": (1280, 720), "medium": (640, 480), "low": (320, 240)}
+if args.resolution in resolutions:
+    if args.use_usb:
+        w, h = resolutions[args.resolution]
+        camera.set(3, w)
+        camera.set(4, h)
+    else:
+        camera.resolution = resolutions[args.resolution]
+else:
+    raise Exception("%s not in resolution options." % args.resolution)
+
+handlers = [(r"/", IndexHandler), (r"/login", LoginHandler),
+            (r"/websocket", WebSocket),
+            (r"/static/password.txt", ErrorHandler),
+            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': ROOT})]
+application = tornado.web.Application(handlers, cookie_secret=PASSWORD)
+application.listen(args.port)
+
+webbrowser.open("http://localhost:%d/" % args.port, new=2)
+
+tornado.ioloop.IOLoop.instance().start()
