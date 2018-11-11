da641da73cbe1bf83ef3547ac9d929991172d90a
apollowebdesigns <andrewevans0@outlook.com>
2018-11-11 14:30:53 +0000

    testing

--- mlserver.py
+++ mlserver.py
@@ -86,6 +86,7 @@ ROOT = os.path.normpath(os.path.dirname(__file__))
 with open(os.path.join(ROOT, "password.txt")) as in_file:
     PASSWORD = in_file.read().strip()
 COOKIE_NAME = "camp"
+camera = PiCamera()
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -135,56 +136,6 @@ class WebSocket(tornado.websocket.WebSocketHandler):
             print("Unsupported function: " + message)
 
     def loop(self):
-        camera = PiCamera()
-        camera.resolution = (IM_WIDTH,IM_HEIGHT)
-        camera.framerate = 10
-        rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
-        rawCapture.truncate(0)
-
-        for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-
-            t1 = cv2.getTickCount()
-            
-            # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-            # i.e. a single-column array, where each item in the column has the pixel RGB value
-            frame = frame1.array
-            frame.setflags(write=1)
-            frame_expanded = np.expand_dims(frame, axis=0)
-
-            # Perform the actual detection by running the model with the image as input
-            (boxes, scores, classes, num) = sess.run(
-                [detection_boxes, detection_scores, detection_classes, num_detections],
-                feed_dict={image_tensor: frame_expanded})
-
-            # Draw the results of the detection (aka 'visulaize the results')
-            vis_util.visualize_boxes_and_labels_on_image_array(
-                frame,
-                np.squeeze(boxes),
-                np.squeeze(classes).astype(np.int32),
-                np.squeeze(scores),
-                category_index,
-                use_normalized_coordinates=True,
-                line_thickness=8,
-                min_score_thresh=0.40)
-
-            cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
-
-            # All the results have been drawn on the frame, so it's time to display it.
-            cv2.imshow('Object detector', frame)
-
-            t2 = cv2.getTickCount()
-            time1 = (t2-t1)/freq
-            frame_rate_calc = 1/time1
-
-            # Press 'q' to quit
-            if cv2.waitKey(1) == ord('q'):
-                break
-
-            rawCapture.truncate(0)
-
-        camera.close()
-
-
         """Sends camera images in an infinite loop."""
         sio = io.BytesIO()
 
@@ -193,6 +144,54 @@ class WebSocket(tornado.websocket.WebSocketHandler):
             img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             img.save(sio, "JPEG")
         else:
+            
+            camera.resolution = (IM_WIDTH,IM_HEIGHT)
+            camera.framerate = 10
+            rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
+            rawCapture.truncate(0)
+
+            for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+
+                t1 = cv2.getTickCount()
+                
+                # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+                # i.e. a single-column array, where each item in the column has the pixel RGB value
+                frame = frame1.array
+                frame.setflags(write=1)
+                frame_expanded = np.expand_dims(frame, axis=0)
+
+                # Perform the actual detection by running the model with the image as input
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: frame_expanded})
+
+                # Draw the results of the detection (aka 'visulaize the results')
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    frame,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8,
+                    min_score_thresh=0.40)
+
+                cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+
+                # All the results have been drawn on the frame, so it's time to display it.
+                cv2.imshow('Object detector', frame)
+
+                t2 = cv2.getTickCount()
+                time1 = (t2-t1)/freq
+                frame_rate_calc = 1/time1
+
+                # Press 'q' to quit
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+                rawCapture.truncate(0)
+
+            camera.close()
             camera.capture(sio, "jpeg", use_video_port=True)
 
         try:
